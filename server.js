9bcfff829a39253f0896731ba4bc18caef001ea1
ArunThiruvenkadam <arun.thiruvenkadam@object-frontier.com>
2014-11-04 17:54:28 +0530

    logger added

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ if (cluster.isMaster) {
 function displayForm(res) {
 	res.writeHeader(200, {"Content-Type": "text/html"});
     res.write(
-        '<form action="/upload" method="post" enctype="multipart/form-data">'+
+        'Please choose PDF : <form action="/upload" method="post" enctype="multipart/form-data">'+
         '<input type="file" name="upload">'+
         '<input type="submit" value="Upload">'+
         '</form>'
